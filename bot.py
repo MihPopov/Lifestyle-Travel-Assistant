98e544ce650e43e97e6348b894b4feeb9834167b
MihPopov <mihail.popov.2008@gmail.com>
2025-11-26 15:47:31 +0300

    bot update

--- bot.py
+++ bot.py
@@ -4,6 +4,12 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from dotenv import load_dotenv
 import os
+import uuid
+import requests
+
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
 
 load_dotenv()
 
@@ -11,18 +17,49 @@ TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+threads = {}
+server_url = "http://localhost:8001"
 
+@tool
+def add_function(a: int, b: int) -> int:
+    """Складывает два числа."""
+    return a + b
+
+@tool
+def mult_function(a: int, b: int) -> int:
+    """Перемножает два числа."""
+    return a * b
+
+tools = [add_function, mult_function]
+model = ChatOpenAI(base_url="http://localhost:8000/v1", model="Qwen/Qwen2.5-7B-Instruct", api_key="dummy")
+agent = create_agent(model=model, tools=tools)
 @dp.message(Command("start"))
 async def cmd_start(message: Message):
+    threads[message.from_user.id] = str(uuid.uuid4())
     await message.answer("Привет! Я Lifestyle Travel Assistant.")
 
-@dp.message(Command("help"))
+@dp.message(Command("clear"))
 async def cmd_start(message: Message):
-    await message.answer("Команда start.")
+    threads[message.from_user.id] = str(uuid.uuid4())
+    await message.answer("История очищена")
 
 @dp.message()
 async def echo(message: Message):
-    await message.answer(message.text)
+    response = requests.post(
+        f"{server_url}/chat",
+        json={
+            "message": message.text,
+            "thread_id": threads.get(message.from_user.id)
+        },
+        timeout=60.0
+    )
+    # result = agent.invoke(
+    #     {"messages": [{"role": "user", "content": message.text}]},
+    #     context={"user_role": "expert"}
+    # )
+    # answer = result["messages"][-1].content
+    data = response.json()["response"]
+    await message.answer(data)
 
 async def main():
     await dp.start_polling(bot)
